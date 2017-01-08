d7e43949eed460d23cca2552b4f1f1bf880e4ec8
Julian <julianbrendl@gmail.com>
2017-01-09 00:38:08 +0100

    added domains

--- env.sh
+++ env.sh
@@ -5,4 +5,9 @@ export DASHBOARD=https://dashboard.sayhi.ai
 export BOTS=https://bots.sayhi.ai
 export PAGE=https://sayhi.ai
 
+export SERVER_DOMAIN=api.sayhi.ai
+export DASHBOARD_DOMAIN=dashboard.sayhi.ai
+export BOTS_DOMAIN=bots.sayhi.ai
+export PAGE_DOMAIN=sayhi.ai
+
 export LETS_ENCRYPT_EMAIL=vader@sayhi.ai

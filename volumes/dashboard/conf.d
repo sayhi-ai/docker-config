8854a5203c42e32c467f8729adc0c45af1b200ad
Julian <julianbrendl@gmail.com>
2017-01-09 01:48:07 +0100

    hopefully it works now

--- volumes/dashboard/conf.d
+++
@@ -1,5 +0,0 @@
-server {
-    listen                  80;
-    server_name             dashboard.sayhi.ai;
-    root                    /usr/share/nginx/html;
-}
--- volumes/page/conf.d
+++
@@ -1,5 +0,0 @@
-server {
-    listen                  80;
-    server_name             sayhi.ai;
-    root                    /usr/share/nginx/html;
-}
